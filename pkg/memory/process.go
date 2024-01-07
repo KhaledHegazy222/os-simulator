60f67cc66d6718ddc6de4d6c03e6ff8e7856468c
Mahmoud Abdulshakour <mahmoud.ali1842001@gmail.com>
2024-01-08 00:59:11 +0200

    Refactor memory component

---
+++ pkg/memory/process.go
@@ -0,0 +1,82 @@
+package memory
+
+import (
+	"errors"
+	"fmt"
+)
+
+
+type STATE string
+
+const(
+		Running    STATE = "running"
+		Terminated STATE = "terminated"
+		Ready      STATE = "ready"
+		Blocked    STATE = "blocked"
+)
+
+var (
+	EndOfInstructionsErr = errors.New("reached end of instructions")
+	ProtectionErr        = errors.New("not allowed to access that part of memory")
+)
+
+type PCB struct {
+	Id            int
+	State         STATE
+	PC            int
+	Start         int
+	End           int
+	CodeSize      int
+	ram *RAMMemory
+}
+
+func (p *PCB) getPCBAddress() int {
+	return p.Start
+}
+
+func (p *PCB) getUnparsedCodeAddress() int {
+	return p.getPCBAddress() + PCBSize
+}
+
+func (p *PCB) getVariablesAddress() int {
+	return p.getUnparsedCodeAddress() + p.CodeSize
+}
+
+func (p *PCB) delete() {
+	for i:=p.Start;i<=p.End;i++ {
+		p.ram[i]=""
+	}
+}
+
+// GetNextInstruction retrieve the next instruction of the current process
+func (p *PCB) GetNextInstruction() (string, error) {
+	if p.PC == p.getVariablesAddress() {
+		return "", EndOfInstructionsErr
+	}
+	
+	instruction := p.ram[p.PC]
+	p.PC++
+	return instruction, nil
+}
+
+// SetDataWord put data in memory in the specified location
+func (p *PCB) SetDataWord(virtualLocation int, data int) error {
+	if virtualLocation < 0 || virtualLocation > 3 {
+		return ProtectionErr
+	}
+	
+	physicalLocation := virtualLocation + p.getVariablesAddress()
+	p.ram[physicalLocation] = fmt.Sprint(data)
+	return nil
+}
+
+// GetDataWord retrieve data from memory from the specified location
+func (p *PCB) GetDataWord(virtualLocation int) (string, error) {
+	if virtualLocation < 0 || virtualLocation > 3 {
+		return "", ProtectionErr
+	}
+	
+	physicalLocation := virtualLocation + p.getVariablesAddress()
+	return p.ram[physicalLocation], nil
+}
+
---
+++ pkg/memory/ram_memory.go
@@ -0,0 +1,93 @@
+package memory
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+var UnableToRetrievePCBErr = errors.New("not able to retrieve PCB from memory")
+
+
+
+// RAMMemory represents a Fixed-sized 40 words RAM memory
+type RAMMemory [memorySize]string
+
+
+func (ram *RAMMemory) getNextID() int {
+	nextId++
+	return nextId
+}
+
+func (ram *RAMMemory) isFree(from int, to int) bool {
+	if from > memoryEndAddress || from < memoryStartAddress || to > memoryEndAddress || to < memoryStartAddress {
+		return false
+	}
+
+	for i := from; i <= to; i++ {
+		if ram[i] != "" {
+			return false
+		}
+	}
+	return true
+}
+
+func (ram *RAMMemory) allocateProcess(start int, unparsedCode []string) PCB {
+	end := start + getProcessSize(len(unparsedCode))
+
+	pcb := PCB{
+		Id:       ram.getNextID(),
+		State:    Running,
+		PC:       start + PCBSize,
+		Start:    start,
+		End:      end,
+		CodeSize: len(unparsedCode),
+	}
+
+	// allocate pcb in the first 6 words
+	pcbAddress:=pcb.getPCBAddress()
+	ram[pcbAddress] = fmt.Sprint(pcb.Id)
+	ram[pcbAddress+1] = fmt.Sprint(pcb.State)
+	ram[pcbAddress+2] = fmt.Sprint(pcb.PC)
+	ram[pcbAddress+3] = fmt.Sprint(pcb.Start)
+	ram[pcbAddress+4] = fmt.Sprint(pcb.End)
+	ram[pcbAddress+5] = fmt.Sprint(pcb.CodeSize)
+
+	// allocate unparsed code
+	unparsedCodeStartAddress := pcb.getUnparsedCodeAddress()
+	for i := 0; i < len(unparsedCode); i++ {
+		ram[unparsedCodeStartAddress+i] = unparsedCode[i]
+	}
+
+	// allocate variables in the last three words. initial value is zero
+	variablesStartAddress := pcb.getVariablesAddress()
+	ram[variablesStartAddress] = fmt.Sprint(0)
+	ram[variablesStartAddress+1] = fmt.Sprint(0)
+	ram[variablesStartAddress+2] = fmt.Sprint(0)
+
+	return pcb
+}
+
+func (ram *RAMMemory) getProcessPCB(startLocation int) (PCB, error) {
+
+	id, idErr := strconv.Atoi(ram[startLocation])
+	pc, pcErr := strconv.Atoi(ram[startLocation+2])
+	start, startErr := strconv.Atoi(ram[startLocation+3])
+	end, endErr := strconv.Atoi(ram[startLocation+4])
+	codeSize, codeSizeErr := strconv.Atoi(ram[startLocation+5])
+
+	if idErr != nil || pcErr != nil || startErr != nil || endErr != nil || codeSizeErr != nil {
+		return PCB{}, UnableToRetrievePCBErr
+	}
+
+	pcb := PCB{
+		Id:       id,
+		State:    STATE(ram[startLocation+1]),
+		PC:       pc,
+		Start:    start,
+		End:      end,
+		CodeSize: codeSize,
+		ram: ram,
+	}
+	return pcb, nil
+}
