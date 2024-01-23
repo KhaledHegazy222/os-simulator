41555f8bd1c099bf817be79915601ca29d627f39
Khaled Hegazy <khaledhegazy870@gmail.com>
2024-01-23 21:10:31 +0200

    Add Methods Documentation

--- pkg/mutex/mutex.go
+++ pkg/mutex/mutex.go
@@ -1,20 +1,28 @@
+// Package mutex provides a simple implementation of a mutex using semaphores.
 package mutex
 
+// Process represents a process identifier.
 type Process int
 
+// Resource represents a resource with an owner process and a list of blocked processes.
 type Resource struct {
 	ownerProcess     Process
 	blockedProcesses []Process
 }
 
+// Mutex represents a mutex that manages resources and provides mutual exclusion.
 type Mutex struct {
 	resources map[string]Resource
 }
 
+// NewMutex creates a new Mutex instance.
 func NewMutex() Mutex {
 	return Mutex{resources: map[string]Resource{}}
 }
 
+// SemWait acquires a lock on the specified resource.
+// If the resource is already locked, the calling process is added to the list of blocked processes.
+// Returns true if the lock is acquired, false otherwise.
 func (m *Mutex) SemWait(targetResource string, process Process) bool {
 	// Lock
 	resource, isPresent := m.resources[targetResource]
@@ -32,6 +40,9 @@ func (m *Mutex) SemWait(targetResource string, process Process) bool {
 	}
 }
 
+// SemSignal releases the lock on the specified resource.
+// If the calling process is the owner of the resource, it releases the lock and updates the state of blocked processes.
+// Returns true if the lock is released, false otherwise.
 func (m *Mutex) SemSignal(targetResource string, process Process) bool {
 	// Release
 	resource, isPresent := m.resources[targetResource]
@@ -45,5 +56,4 @@ func (m *Mutex) SemSignal(targetResource string, process Process) bool {
 	} else {
 		return false
 	}
-
 }
